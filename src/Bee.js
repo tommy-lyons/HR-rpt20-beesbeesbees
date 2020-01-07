5adc80b34a73345ba57aa05660d5d6489948b138
Thomas Lyons <talyons22@gmail.com>
2020-01-07 11:50:44 -0800

    Complete Grub.js, Implementing Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,36 @@
 var Bee = function() {
+
+  var bee = new Grub;
+
+  // grub.call(this, food, eat);
+  bee.age = 5;
+  bee.color = 'yellow';
+  bee.job = 'keep on growing';
+
+  return bee;
 };
 
+// Bee.prototype.age = 3;
+// Bee.prototype.color = 'yelow';
+// Bee.prototype.job = 'keep on growing';
+
+
+
+
+// var Car = function () {
+//   this.loc = loc;
+// }
+
+// var amy = new Car(1)
+
+
+
+// Create a Bee class, in pseudoclassical style, with:
+//  call the Grub superclass
+//  set the prototype
+//  set the constructor
+//  an age property that is set to 5
+//  a color property that is set to yellow
+//  a food property that is inherited from grub
+//  an eat method that is inherited from grub
+//  a job property that is set to keep on growing
--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,33 @@
 var Grub = function() {
+
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+
 };
 
+Grub.prototype.eat = function(value){
+
+}
+
+
+
+
+// Sub Classing
+// Build your first class in Grub.js. Grub will act as the superclass for all
+// other types of bees. Work through the repo in the order specified below,
+// making sure to pass all of the Specs in test/index.html --which you can
+// run by simply opening it in your browser and refreshing the page whenever
+// you make changes.
+
+// Things To Note:
+// This assignment must be written in pseudoclassical style.
+// Although there are multiple tests for each class, you will only be able to see
+// one test at a time. As soon as one test fails the spec runner stops running and
+// you must pass the current test to continue.
+
+// Create a Grub class, in pseudoclassical style, with:
+// an age property that is set to 0
+// a color property that is set to pink
+// a food property that is set to jelly
+// an eat method
